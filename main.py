db5fd4790c7cc426419b90191afaf6044f20f558
Ajapson <165334540+Ajapson@users.noreply.github.com>
2025-05-25 14:27:50 -0400

    feat: added XGBoost model creation

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 def main():
     print("Hello from gr11-soo!")
 
+# Ajapson Code #
+import xgboost as xgb
+
+model = xgb.XGBClassifier()
+print("XGBoost model created")
 
 if __name__ == "__main__":
     main()
+
+
+
